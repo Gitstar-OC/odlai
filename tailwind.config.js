b7cc8eff940a7798c385fda7c0909ab466a37904
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-10-31 13:06:14 +0530

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,24 @@ export default {
       },
     },
   },
-  presets: [createPreset()],
+presets: [
+    createPreset({
+      addGlobalColors: true,
+      preset: {
+        ...presets.default,
+        dark: {
+          ...presets.default.dark,
+          background: '0 0% 2%',
+          foreground: '0 0% 98%',
+          popover: '0 0% 4%',
+          card: '0 0% 4%',
+          muted: '0 0% 8%',
+          border: '0 0% 14%',
+          accent: '0 0% 15%',
+          'accent-foreground': '0 0% 100%',
+          'muted-foreground': '0 0% 60%',
+        },
+      },
+    }),
+  ],
 };
