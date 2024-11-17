0feaeb4cbf27b0db25e67575e6dd0c190f377f34
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-11-18 00:44:14 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,21 @@
 import "./global.css";
 import type { ReactNode } from "react";
 import { GeistSans } from "geist/font/sans";
-// import type { Viewport } from "next";
 import { GeistMono } from "geist/font/mono";
-import { baseUrl, bannerUrl } from "@/utils/metadata";
-import type { Metadata } from "next/types";
+import { baseUrl, createMetadata, bannerUrl } from "@/utils/metadata";
 import { RootProvider } from "fumadocs-ui/provider";
 // import { Banner } from "fumadocs-ui/components/banner";
 // import Link from "next/link";
 
-export const metadata: Metadata = {
+export const metadata = createMetadata({
   title: {
     default: "OpenDeepLearning",
     template: "%s | OpenDeepLearning",
   },
   description:
-    "Courses, code, news, research, and community for building AI-powered products.",
-  twitter: {
-    card: "summary_large_image",
-  },
-  openGraph: {
-    images: "/banner.png",
-    title: {
-      default: "OpenDeepLearning",
-      template: "%s | OpenDeepLearning",
-    },
-    description:
-      "Courses, code, news, research, and community for building AI-powered products.",
-  },
+    "Courses, code, news, research, and community for building AI-powered products..",
   metadataBase: baseUrl,
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
-
-// export const viewport: Viewport = {
-//   themeColor: [
-//     { media: "(prefers-color-scheme: dark)", color: "#0A0A0A" },
-//     { media: "(prefers-color-scheme: light)", color: "#fff" },
-//   ],
-// };
+});
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
--- utils/metadata.ts
+++ utils/metadata.ts
@@ -1,7 +1,34 @@
+import type { Metadata } from "next/types";
+
+export function createMetadata(override: Metadata): Metadata {
+  return {
+    ...override,
+    openGraph: {
+      title: override.title ?? undefined,
+      description: override.description ?? undefined,
+      url: "https://sunar.vercel.app",
+      images: "/banner.png",
+      siteName: "OpenDeepLearning",
+      ...override.openGraph,
+    },
+    twitter: {
+      card: "summary_large_image",
+      creator: "@1msirius_",
+      title: override.title ?? undefined,
+      description: override.description ?? undefined,
+      images: "/banner.png",
+      ...override.twitter,
+    },
+    icons: {
+      icon: "/favicon.ico",
+    },
+  };
+}
+
 export const baseUrl =
-  process.env.NODE_ENV === "development" || !process.env.VERCEL_URL
+  process.env.NODE_ENV === "development"
     ? new URL("http://localhost:3000")
-    : new URL(`https://${process.env.VERCEL_URL}`);
+    : new URL(`https://${process.env.VERCEL_URL!}`);
 
 export const bannerUrl =
   "https://github.com/OpenDeepLearningAI/OpenDeepLearning";
