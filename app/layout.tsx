b4fd94915947ebf36de27983327c443f69563d19
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-11-20 03:06:30 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,9 @@ import { GeistMono } from "geist/font/mono";
 import { baseUrl, bannerUrl } from "@/utils/metadata";
 import { RootProvider } from "fumadocs-ui/provider";
 import type { Metadata } from "next/types";
-// import { Banner } from "fumadocs-ui/components/banner";
-// import Link from "next/link";
-// import { FaGithubAlt } from "react-icons/fa6";
-import Preview from "./components/preview/banner";
+import { Banner } from "fumadocs-ui/components/banner";
+import Link from "next/link";
+import { FaGithubAlt } from "react-icons/fa6";
 
 
 export const metadata: Metadata = {
@@ -58,7 +57,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       suppressHydrationWarning
     >
       <body className="flex flex-col min-h-screen">
-        {/* <Banner variant="rainbow" id="hello-world">
+        <Banner variant="rainbow" id="hello-world">
           <Link
             href={bannerUrl}
             target="_blank"
@@ -68,8 +67,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             ⭐ Loving ODL? Leave a star!{" "}
             <FaGithubAlt className="ml-2 text-lg" />
           </Link>
-        </Banner> */}
-        <Preview />
+        </Banner>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
