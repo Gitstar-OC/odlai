c3caf257818648e2ed831f829f1bfa629417f699
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-11-20 02:27:14 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,52 @@
-import 'katex/dist/katex.css';
+import "katex/dist/katex.css";
 import "./global.css";
 import type { ReactNode } from "react";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
-import { baseUrl, createMetadata, bannerUrl } from "@/utils/metadata";
+import { baseUrl, bannerUrl } from "@/utils/metadata";
 import { RootProvider } from "fumadocs-ui/provider";
+import type { Metadata } from "next/types";
 import { Banner } from "fumadocs-ui/components/banner";
 import Link from "next/link";
 import { FaGithubAlt } from "react-icons/fa6";
 
-export const metadata = createMetadata({
+export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
   title: {
     default: "OpenDeepLearning",
     template: "%s | OpenDeepLearning",
   },
   description:
-    "Courses, code, news, research, and community for building AI-powered products..",
-  metadataBase: baseUrl,
-});
+    "Courses, code, news, research, and community for building AI-powered products",
+  openGraph: {
+    images: "/banner.png",
+    title: "OpenDeepLearning",
+    description:
+      "Courses, code, news, research, and community for building AI-powered products.",
+    url: baseUrl,
+    siteName: "OpenDeepLearning",
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "@ODL_AI",
+    card: "summary_large_image",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
