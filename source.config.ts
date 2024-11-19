773e2b1ae45b1eb88ff52c0dbca3dc5c4d940310
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-11-19 19:10:11 +0530

    Update

--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,14 @@
 import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 export const { docs, meta } = defineDocs({
   dir: 'content/docs',
 });
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: (v) => [rehypeKatex, ...v],
+  },
+});
